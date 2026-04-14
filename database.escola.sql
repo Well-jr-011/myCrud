c2443b03e1e54b77430d6ff31081a88ff6dbda32
well-jr-011 <wellingtonoliveira0901@gmail.com>
2026-04-14 20:29:57 -0300

    database

--- database.escola.sql
+++ database.escola.sql
@@ -10,3 +10,5 @@ CREATE TABLE professores (
     nome TEXT NOT NULL,
     disciplina TEXT
 );
+git config --global user.email "wellingtonoliveira0901@gmail.com"
+git config --global user.name "well-jr-011"
